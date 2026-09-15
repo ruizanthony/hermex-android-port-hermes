b2ba317e7eb13e8469d9fd3a7eb5efe0e81cee3e
Anthony Ruiz <ruizanthony@users.noreply.github.com>
2026-09-15 17:42:37 +0000

    chore(release): 1.2.0-pichot.7 (versionCode 41)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.uzairansar.hermex.pichot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 40
-        versionName = "1.2.0-pichot.6"
+        versionCode = 41
+        versionName = "1.2.0-pichot.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
