8af654d0d947e9fe5e0d11a05600093ca18b6bc4
anthony <anthony@pichot.commit>
2026-09-17 12:46:02 +0000

    Release 1.2.0-pichot.10: versionCode 44, foreground refresh + background warm loop + spinner

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.uzairansar.hermex.pichot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 43
-        versionName = "1.2.0-pichot.9"
+        versionCode = 44
+        versionName = "1.2.0-pichot.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
